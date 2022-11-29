728116f39c6cf798c46b257c8b62547420f3f304
Rodrigo Jiménez <rodrigo.jimenez@alumnos.ucn.cl>
2022-11-28 22:56:06 -0300

    feat: Added protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './lib/context/auth.context';
 import Header from './components/Header';
+import React from 'react';
+import ProtectedRoute from './components/utils/ProtectedRoute'
+import Dictionary from './pages/admin/Dictionary';
+import Game from './pages/admin/Game';
+import News from './pages/admin/News';
+import Users from './pages/admin/Users';
 
 function App() {
   return (
-    <div>
-        <Header>Portal de Noticia</Header>
-    </div>
+    <AuthProvider>
+      <Router>
+      <Routes>
+            <Route path='/' element={ <Home /> }/>
+            <Route path='/login' element={ <Login /> }/>
+            <Route path='/admin/dictionary' element={
+              <ProtectedRoute>
+                <Dictionary />
+              </ProtectedRoute>
+            }/>
+            <Route path='/admin/news' element={
+              <ProtectedRoute>
+                <News />
+              </ProtectedRoute>
+            }/>
+            <Route path='/admin/users' element={
+              <ProtectedRoute>
+                <Users />
+              </ProtectedRoute>
+            }/>
+            <Route path='/admin/game' element={
+              <ProtectedRoute>
+                <Game />
+              </ProtectedRoute>
+            }/>
+            <Route path='*' component={<h1>404 Page Not Found</h1>}/>
+          </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
