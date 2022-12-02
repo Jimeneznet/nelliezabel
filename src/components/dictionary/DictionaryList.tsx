ddcafacc85340bacfedb3c8a6b430cc9194f88b2
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-02 15:39:42 -0300

    feat: pagination of the word list was added (4 words per page were set)

--- src/components/dictionary/DictionaryList.tsx
+++ src/components/dictionary/DictionaryList.tsx
@@ -1,14 +1,26 @@
 import { Word } from "@lib/types/word.types";
 import DictionaryRow from "./DictionaryRow";
+import { usePagination } from "hooks/dictionary/pagination.hooks";
+import Pagination from "../dictionary/Pagination";
 
 const DictionaryList = ({ words }: { words: Word[] }) => {
+  const { currentWords, wordsPerPage, currentPage, paginate } =
+    usePagination(words);
   return (
-    <div className="flex justify-center">
-      <ul className=" w-1/2 mt-6">
-        {words.map((word, index) => (
-          <DictionaryRow key={index} word={word} />
-        ))}
-      </ul>
+    <div>
+      <div className="flex justify-center">
+        <ul className=" w-1/2 mt-10">
+          {currentWords.map((word, index) => (
+            <DictionaryRow key={index} word={word} />
+          ))}
+        </ul>
+      </div>
+      <Pagination
+        totalWords={words.length}
+        wordsPerPage={wordsPerPage}
+        currentPage={currentPage}
+        paginate={paginate}
+      />
     </div>
   );
 };
