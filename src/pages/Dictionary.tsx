59f3bb3836139c32419f219f0b32de736b72a949
Lelouch96 <luis-cubillos96@outlook.cl>
2022-12-03 00:51:04 -0300

    feat: datalist of the words in firebase

--- src/pages/Dictionary.tsx
+++ src/pages/Dictionary.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Header from "../components/Header";
+import Datalist from "../components/Datalist"
 
 const Dictionary = () => {
   return (
     <div>
       <div className="">
         <Header>Centro de Administracion</Header>
+        <Datalist></Datalist>
         <Layout>Diccionario</Layout>
       </div>
     </div>
