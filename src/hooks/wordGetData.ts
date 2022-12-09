8247a6c509ae50b113253bb0fc249752b5b63ccd
Rodrigo Jiménez <rodrigo.jimenez@alumnos.ucn.cl>
2022-12-09 00:08:08 -0300

    fix: deleted unused hook wordGetData

--- src/hooks/wordGetData.ts
+++
@@ -1,9 +0,0 @@
-import { collection, getDocs } from "firebase/firestore";
-import { db } from '../lib/config/firebase.config';
-
-async function getData() {
-    const q = await getDocs(collection(db,'words'));
-    return q.docs;
-}
-
-export {getData}
