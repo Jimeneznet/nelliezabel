a71b1344ff5b05ecf9b83c2034e8686e2a697981
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-17 13:13:09 -0300

    feat: word history list and hooks was added

--- src/components/dictionary/DictionarySearchInput.tsx
+++ src/components/dictionary/DictionarySearchInput.tsx
@@ -1,18 +1,33 @@
 export interface IDictionarySearchInput {
   handleSearch: () => void;
   handleInputChange: (value: string) => void;
+  history: string[];
+  searchQuery: string;
 }
 
 const DictionarySearchInput: React.FC<IDictionarySearchInput> = (props) => {
   return (
     <div className="flex gap-2 w-full flex-col md:flex-row my-4">
       <div className="flex-1">
-        <input
-          type="text"
-          placeholder="Escribe una palabra"
-          className="input input-bordered w-full"
-          onChange={(event) => props.handleInputChange(event.target.value)}
-        />
+        <div className="dropdown w-full">
+          <input
+            type="text"
+            placeholder="Escribe una palabra"
+            className="input input-bordered w-full"
+            onChange={(event) => props.handleInputChange(event.target.value)}
+            value={props.searchQuery}
+          />
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-full"
+          >
+            {props.history.map((word, index) => (
+              <li key={index}>
+                <p onClick={() => props.handleInputChange(word)}>{word}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div>
         <button
