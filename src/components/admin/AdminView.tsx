7c0437b130594f47fa6b3a860e6783ea5272fc91
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-20 15:57:15 -0300

    feat: loading bar was added to admin view

--- src/components/admin/AdminView.tsx
+++ src/components/admin/AdminView.tsx
@@ -1,21 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
-
-import { getData } from '../../hooks/userGetData'
-import {updateUserStatus} from '../../hooks/userUpdateStatus'
-
+import LoadingBar from "../LoadingBar";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getData } from "../../hooks/userGetData";
+import { updateUserStatus } from "../../hooks/userUpdateStatus";
 
 const AdminView = () => {
   const [usuarios, setUsuarios] = useState<any[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-
-    setLoading(true)
-
     const handleGetUser = async () => {
       const userDocs = await getData();
-      console.log(userDocs)
       const sanatizedUsers = userDocs.map((doc) => ({
         nombre: doc.data().nombre,
         rut: doc.data().rut,
@@ -23,55 +18,78 @@ const AdminView = () => {
         email: doc.data().email,
         rol: doc.data().rol,
         status: doc.data().status,
-        uid: doc.data().uid
-      }))
+        uid: doc.data().uid,
+      }));
 
       setUsuarios(sanatizedUsers);
-      setLoading(false);
-    }
+      setIsLoading(false);
+    };
 
     handleGetUser();
-
-  }, [])
+  }, []);
 
   return (
     <div>
-      <Link to="/register"><button className='btn'>Agregar</button></Link>
-      <div className="overflow-x-auto">
-        <table className="table w-full">
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Rut</th>
-              <th>Correo</th>
-              <th>Rol</th>
-              <th>Status</th>
-              <th>Editar</th>
-              <th>Eliminar</th>
-            </tr>
-          </thead>
-          <tbody>
-          {usuarios.map((user: any, index: number) => (
+      {isLoading ? (
+        <LoadingBar />
+      ) : (
+        <div>
+          <Link to="/register">
+            <button className="btn">Agregar</button>
+          </Link>
+          <div className="overflow-x-auto">
+            <table className="table w-full">
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Rut</th>
+                  <th>Correo</th>
+                  <th>Rol</th>
+                  <th>Status</th>
+                  <th>Editar</th>
+                  <th>Eliminar</th>
+                </tr>
+              </thead>
+              <tbody>
+                {usuarios.map((user: any, index: number) => (
                   <tr key={index}>
                     <td>{user.nombre}</td>
                     <td>{user.rut}</td>
                     <td>{user.email}</td>
                     <td>{user.rol}</td>
                     <td>{user.status}</td>
-                    <td><Link className='btn' to={`/admin/users/edit/${user.uid}`}>editar</Link></td>
                     <td>
-                      <button 
-                        className='btn' 
-                        onClick={() => updateUserStatus(user.uid,user.status, usuarios, setUsuarios)}> 
+                      <Link
+                        className="btn"
+                        to={`/admin/users/edit/${user.uid}`}
+                      >
+                        editar
+                      </Link>
+                    </td>
+                    <td>
+                      <button
+                        className="btn"
+                        onClick={() =>
+                          updateUserStatus(
+                            user.uid,
+                            user.status,
+                            usuarios,
+                            setUsuarios
+                          )
+                        }
+                      >
                         cambiar status
                       </button>
                     </td>
-                  </tr>))}
-          </tbody>
-        </table>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default AdminView
+export default AdminView;
