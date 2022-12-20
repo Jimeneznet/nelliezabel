d59123f6e4b7eaa2bd7da31fa50b0174609e3f94
Lelouch96 <luis-cubillos96@outlook.cl>
2022-12-20 19:48:34 -0300

    feat: category filter

--- src/components/utils/Filter.tsx
+++ src/components/utils/Filter.tsx
@@ -4,25 +4,84 @@ export const filterQuery = (sentence: string) => {
   const search = sentence
     .toLocaleLowerCase()
     .split(" ")
-    .map((element: any) => element.replace(/[^a-zA-Z]/g, ""))
+    .map((element: any) => element.replace(/[^a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+/g, ""))
     .filter((item) => item !== "");
   return search;
 };
+export const filterCategory = (isCheckedA: boolean, isCheckedB: boolean, isCheckedC: boolean ) => {
+  let categories = ["Educación","Jurídico","Psicología"];
+  (isCheckedA === true && isCheckedB === true && isCheckedC === true) ? console.log("without filters") :
+  (isCheckedA === true && isCheckedB === true) ? categories.splice(2,1) : (isCheckedB === true && isCheckedC === true) ? categories.splice(0,1) :
+  (isCheckedA === true && isCheckedC === true) ? categories.splice(1,1) : isCheckedA === true ? categories=categories.filter(tech => tech === "Educación") :
+  isCheckedB === true  ? categories=categories.filter(tech => tech === "Jurídico")  : isCheckedC === true  ? categories=categories.filter(tech => tech === "Psicología") : console.log("without filters")
+  return categories;
+};
 
-export const filterWords = (words: Word[], searchQuery: string) => {
-  if (searchQuery === null || searchQuery.match(/^ *$/) !== null) return words;
+export const filterWords = (words: Word[], searchQuery: string, isCheckedA: boolean, isCheckedB: boolean, isCheckedC: boolean ) => {
+  const categories = filterCategory(isCheckedA,isCheckedB,isCheckedC)
+  console.log(categories)
+  const [categoryOne,categoryTwo] = categories
   const parsedFilter = filterQuery(searchQuery);
   let parsedWords: Word[] = [];
-  for (let i = 0; i < words.length; i++) {
-    const element = filterQuery(words[i].word);
-    for (let j = 0; j < element.length; j++) {
-      const query = element[j];
-      if (parsedFilter.includes(query)) {
-        parsedWords.push(words[i]);
-        break;
+  
+
+  if ((searchQuery === null || searchQuery.match(/^ *$/) !== null) && categories.length === 3 ) return words;
+
+  if ((searchQuery === null || searchQuery.match(/^ *$/) !== null) && categories.length === 2 ) {
+    for (let i = 0; i < words.length; i++) {
+      const category= filterQuery(words[i].category);
+      for (let j = 0; j < category.length; j++) {
+        if (categoryOne.toLocaleLowerCase() === category[j] || categoryTwo.toLocaleLowerCase() === category[j]) {
+          parsedWords.push(words[i]);
+          break;
+        }
       }
     }
+    return parsedWords;
+  } else if ((searchQuery === null || searchQuery.match(/^ *$/) !== null) && categories.length === 1 ) {
+    
+    for (let i = 0; i < words.length; i++) {
+      const category= filterQuery(words[i].category);
+      for (let j = 0; j < category.length; j++) {
+        if (categoryOne.toLocaleLowerCase() === category[j]) {
+          parsedWords.push(words[i]);
+          break;
+        }
+      }
+    }
+    console.log(parsedWords);
+    
+    return parsedWords;
   }
 
-  return parsedWords;
+  if (categories.length === 2 ) {
+    for (let i = 0; i < words.length; i++) {
+      const element = filterQuery(words[i].word);
+      const category= filterQuery(words[i].category);
+      for (let j = 0; j < element.length; j++) {
+        const query = element[j];
+        if (parsedFilter.includes(query) && categoryOne.toLocaleLowerCase() === category[j] || categoryTwo.toLocaleLowerCase() === category[j]) {
+          parsedWords.push(words[i]);
+          break;
+        }
+      }
+    }
+    return parsedWords;
+  } else if (categories.length === 1 ) {
+    for (let i = 0; i < words.length; i++) {
+      const element = filterQuery(words[i].word);
+      const category= filterQuery(words[i].category);
+      for (let j = 0; j < element.length; j++) {
+        const query = element[j];
+        if (parsedFilter.includes(query) && categoryOne.toLocaleLowerCase() === category[j]) {
+          parsedWords.push(words[i]);
+          break;
+        }
+      }
+    }
+    return parsedWords;
+  }
+  
+  
 };
+
--- src/pages/Dictionary.tsx
+++ src/pages/Dictionary.tsx
@@ -1,20 +1,30 @@
+import * as React from "react";
 import DictionaryList from "../components/dictionary/DictionaryList";
 import { useWord } from "api/dictionary/dictionary.api";
 import Header from "../components/Header";
 import { useEffect, useState } from "react";
-import { filterWords } from "../components/utils/Filter";
+import { filterWords} from "../components/utils/Filter";
 import { Word } from "@lib/types/word.types";
 import DictionarySearchInput from "../components/dictionary/DictionarySearchInput";
+import Checkbox from "../components/utils/Checkbox";
+
+
+
 
 const Dictionary = () => {
   const { words } = useWord();
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredWords, setFilteredWords] = useState<Word[]>([]);
+  const [isCheckedA, setIsCheckedA] = useState(false);
+  const [isCheckedB, setIsCheckedB] = useState(false);
+  const [isCheckedC, setIsCheckedC] = useState(false);
+
   useEffect(() => {
     // Update the document title using the browser API
-    const filtered = filterWords(words, searchQuery);
+    const filtered = filterWords(words, searchQuery, isCheckedA, isCheckedB, isCheckedC);
+    if (filtered){
     setFilteredWords(filtered);
-
+    }
     // ATENCION: Se desactiva el linter ya que no queremos filtrar cada vez que cambia el input
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [words]);
@@ -24,12 +34,54 @@ const Dictionary = () => {
   };
 
   const handleSearch = () => {
-    setFilteredWords(filterWords(words, searchQuery));
+    const filtered = filterWords(words, searchQuery, isCheckedA, isCheckedB, isCheckedC);
+    if (filtered){
+      setFilteredWords(filtered);
+    }
   };
+  
+  const handleChangeA = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsCheckedA(e.target.checked);
+  };
+  
+  const handleChangeB = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsCheckedB(e.target.checked);
+  };
+
+  const handleChangeC = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsCheckedC(e.target.checked);
+  };
+
+
   return (
     <div>
       <div className="">
         <Header>Diccionario</Header>
+        <div className="block m-auto w-3/4 py-8 ">
+          <label htmlFor="">Filtros</label>
+          
+        <Checkbox
+            handleChange={handleChangeA}
+            isChecked={isCheckedA}
+            label="Educación"
+          />
+          <Checkbox
+            handleChange={handleChangeB}
+            isChecked={isCheckedB}
+            label="Jurídico"
+          />
+        
+        
+          <Checkbox
+            handleChange={handleChangeC}
+            isChecked={isCheckedC}
+            label="Psicología"
+          />
+        </div>
+         
+        
+        
+          
         <div className="block m-auto w-3/4 py-8">
           <DictionarySearchInput
             handleSearch={handleSearch}
