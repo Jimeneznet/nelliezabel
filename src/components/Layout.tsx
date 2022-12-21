20f80b23f0d097b784cc5ea860f42143be93f132
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-21 13:55:00 -0300

    fix: height of the layout was fixed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,10 +2,10 @@ import SideNavbar from "./SideNavbar";
 
 const Layout = ({ children }: any) => {
   return (
-    <div className="h-screen flex justify-start">
+    <div className="min-h-screen flex justify-start">
       <SideNavbar />
       <div className="bg-white flex-1">
-        <div className="h-200px">{children}</div>
+        <div>{children}</div>
       </div>
     </div>
   );
