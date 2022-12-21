b7db9f09495690bd94c85355d4cac5ccb16a114d
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-20 21:35:05 -0300

    refactor: user state was removed in update status

--- src/hooks/userUpdateStatus.ts
+++ src/hooks/userUpdateStatus.ts
@@ -5,15 +5,9 @@ import {
   where,
   updateDoc,
 } from "firebase/firestore";
-import { Dispatch, SetStateAction } from "react";
 import { db } from "../lib/config/firebase.config";
 
-const updateUserStatus = async (
-  uid: any,
-  status: any,
-  users: any[],
-  setUsers: Dispatch<SetStateAction<any[]>>
-) => {
+const updateUserStatus = async (uid: any, status: any) => {
   try {
     const q = query(collection(db, "usuarios"), where("uid", "==", uid));
     const docs = await getDocs(q);
@@ -24,14 +18,6 @@ const updateUserStatus = async (
         await updateDoc(userRef, {
           status: status === "1" ? "0" : "1",
         });
-        const newUsersState = users.map((user) => {
-          if (uid !== user.uid) {
-            return user;
-          }
-          return { ...user, status: status === "1" ? "0" : "1" };
-        });
-
-        setUsers(newUsersState);
       }
     }
   } catch (error) {
