75535de8cbe4d6d421249e711763fcd5ba1cb7bd
Kwwa <rodrigo.jimenez@alumnos.ucn.cl>
2022-12-22 20:53:53 -0300

    feat: refactored filer.tsx and created style for filters

--- src/components/utils/Filter.tsx
+++ src/components/utils/Filter.tsx
@@ -8,55 +8,57 @@ export const filterQuery = (sentence: string) => {
     .filter((item) => item !== "");
   return search;
 };
-export const filterCategory = (isCheckedA: boolean, isCheckedB: boolean, isCheckedC: boolean ) => {
-  let categories = ["Educación","Jurídico","Psicología"];
-  (isCheckedA === true && isCheckedB === true && isCheckedC === true) ? console.log("without filters") :
-  (isCheckedA === true && isCheckedB === true) ? categories.splice(2,1) : (isCheckedB === true && isCheckedC === true) ? categories.splice(0,1) :
-  (isCheckedA === true && isCheckedC === true) ? categories.splice(1,1) : isCheckedA === true ? categories=categories.filter(tech => tech === "Educación") :
-  isCheckedB === true  ? categories=categories.filter(tech => tech === "Jurídico")  : isCheckedC === true  ? categories=categories.filter(tech => tech === "Psicología") : console.log("without filters")
+export const filterCategory = (
+  isCheckedA: boolean,
+  isCheckedB: boolean,
+  isCheckedC: boolean
+) => {
+  let categories = ["Educación", "Jurídico", "Psicología"];
+  isCheckedA === true && isCheckedB === true && isCheckedC === true
+    ? console.log("without filters")
+    : isCheckedA === true && isCheckedB === true
+    ? categories.splice(2, 1)
+    : isCheckedB === true && isCheckedC === true
+    ? categories.splice(0, 1)
+    : isCheckedA === true && isCheckedC === true
+    ? categories.splice(1, 1)
+    : isCheckedA === true
+    ? (categories = categories.filter((tech) => tech === "Educación"))
+    : isCheckedB === true
+    ? (categories = categories.filter((tech) => tech === "Jurídico"))
+    : isCheckedC === true
+    ? (categories = categories.filter((tech) => tech === "Psicología"))
+    : console.log("without filters");
   return categories;
 };
 
-export const filterWords = (words: Word[], searchQuery: string, isCheckedA: boolean, isCheckedB: boolean, isCheckedC: boolean ) => {
-  const categories = filterCategory(isCheckedA,isCheckedB,isCheckedC)
-  console.log(categories)
-  const [categoryOne,categoryTwo] = categories
+export const filterCategory2 = (
+  categoriesControl: Array<{ categoryName: string; categoryEnabled: boolean }>
+) => {
+  const filteredCategories = categoriesControl
+    .map((element) => {
+      if (element.categoryEnabled) return element.categoryName;
+    })
+    .filter((element) => {
+      return element != null && element !== undefined && element !== "";
+    });
+  return filteredCategories;
+};
+
+export const filterWords = (
+  words: Word[],
+  searchQuery: string,
+  categoriesControl: Array<{ categoryName: string; categoryEnabled: boolean }>
+) => {
+  const categories = filterCategory2(categoriesControl);
   const parsedFilter = filterQuery(searchQuery);
   let parsedWords: Word[] = [];
-  
 
-  if ((searchQuery === null || searchQuery.match(/^ *$/) !== null) && categories.length === 3 ) return words;
-
-  if ((searchQuery === null || searchQuery.match(/^ *$/) !== null) && categories.length === 2 ) {
-    for (let i = 0; i < words.length; i++) {
-      const category= filterQuery(words[i].category);
-      for (let j = 0; j < category.length; j++) {
-        if (categoryOne.toLocaleLowerCase() === category[j] || categoryTwo.toLocaleLowerCase() === category[j]) {
-          parsedWords.push(words[i]);
-          break;
-        }
-      }
-    }
-    return parsedWords;
-  } else if ((searchQuery === null || searchQuery.match(/^ *$/) !== null) && categories.length === 1 ) {
-    
-    for (let i = 0; i < words.length; i++) {
-      const category= filterQuery(words[i].category);
-      for (let j = 0; j < category.length; j++) {
-        if (categoryOne.toLocaleLowerCase() === category[j]) {
-          parsedWords.push(words[i]);
-          break;
-        }
-      }
-    }
-    console.log(parsedWords);
-    
-    return parsedWords;
-  }
-
-
-
-  if (categories.length === 3 ) {
+  if (searchQuery === null || searchQuery.match(/^ *$/) !== null) {
+    parsedWords = words.filter((element) => {
+      return element != null && element !== undefined;
+    });
+  } else {
     for (let i = 0; i < words.length; i++) {
       const element = filterQuery(words[i].word);
       for (let j = 0; j < element.length; j++) {
@@ -67,35 +69,26 @@ export const filterWords = (words: Word[], searchQuery: string, isCheckedA: bool
         }
       }
     }
-    return parsedWords;
-  } else if (categories.length === 2 ) {
-    for (let i = 0; i < words.length; i++) {
-      const element = filterQuery(words[i].word);
-      const category= filterQuery(words[i].category);
-      for (let j = 0; j < element.length; j++) {
-        const query = element[j];
-        if (parsedFilter.includes(query) && (categoryOne.toLocaleLowerCase() === category[j] || categoryTwo.toLocaleLowerCase() === category[j])) {
-          parsedWords.push(words[i]);
-          break;
-        }
-      }
-    }
-    return parsedWords;
-  } else if (categories.length === 1 ) {
-    for (let i = 0; i < words.length; i++) {
-      const element = filterQuery(words[i].word);
-      const category= filterQuery(words[i].category);
-      for (let j = 0; j < element.length; j++) {
-        const query = element[j];
-        if (parsedFilter.includes(query) && categoryOne.toLocaleLowerCase() === category[j]) {
-          parsedWords.push(words[i]);
-          break;
-        }
-      }
-    }
-    return parsedWords;
   }
-  
-  
-};
+  console.log(categories);
 
+  if (parsedWords.length === 0) return [];
+  if (categories.length !== 3) {
+    for (let i = 0; i < parsedWords.length; i++) {
+      const category = parsedWords[i].category;
+      console.log(category);
+      if (!categories.includes(category)) {
+        delete parsedWords[i];
+      } else {
+        console.log(category);
+      }
+    }
+  }
+
+  parsedWords = parsedWords.filter((element) => {
+    return element != null && element !== undefined;
+  });
+  console.log(parsedWords);
+
+  return parsedWords;
+};
--- src/pages/Dictionary.tsx
+++ src/pages/Dictionary.tsx
@@ -3,14 +3,13 @@ import DictionaryList from "../components/dictionary/DictionaryList";
 import { useWord } from "api/dictionary/dictionary.api";
 import Header from "../components/Header";
 import { useEffect, useState, useRef } from "react";
-import { filterWords} from "../components/utils/Filter";
+import { filterWords } from "../components/utils/Filter";
 import { Word } from "@lib/types/word.types";
 import DictionarySearchInput from "../components/dictionary/DictionarySearchInput";
 import Checkbox from "../components/utils/Checkbox";
 
-
-
 import { useHandleHistory, useHistory } from "hooks/dictionary/history.hooks";
+import { ICategoryFilterList } from "@components/dictionary/DictionaryCategoryFilterList";
 
 const Dictionary = () => {
   const { words } = useWord();
@@ -22,12 +21,29 @@ const Dictionary = () => {
   const [isCheckedA, setIsCheckedA] = useState(false);
   const [isCheckedB, setIsCheckedB] = useState(false);
   const [isCheckedC, setIsCheckedC] = useState(false);
+  const categories = ["Psicología", "Jurídico", "Educación"];
+  const [categoriesControl, setCategories] = useState(
+    categories.map((category) => {
+      return { categoryName: category, categoryEnabled: true };
+    })
+  );
+
+  const categoryToggleCallbackFunction = (categoryName: string) => {
+    const newCategoriesControl = [...categoriesControl];
+    const categoryIndex = newCategoriesControl
+      .map((item) => item.categoryName)
+      .indexOf(categoryName);
+    newCategoriesControl[categoryIndex].categoryEnabled =
+      !newCategoriesControl[categoryIndex].categoryEnabled;
+    setCategories(newCategoriesControl);
+    return;
+  };
 
   useEffect(() => {
     // Update the document title using the browser API
-    const filtered = filterWords(words, searchQuery, isCheckedA, isCheckedB, isCheckedC);
-    if (filtered){
-    setFilteredWords(filtered);
+    const filtered = filterWords(words, searchQuery, categoriesControl);
+    if (filtered) {
+      setFilteredWords(filtered);
     }
     // ATENCION: Se desactiva el linter ya que no queremos filtrar cada vez que cambia el input
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -39,16 +55,21 @@ const Dictionary = () => {
 
   const handleSearch = () => {
     handleHistory(searchQuery, history, setHistory);
-    const filtered = filterWords(words, searchQuery, isCheckedA, isCheckedB, isCheckedC);
-    if (filtered){
+    const filtered = filterWords(words, searchQuery, categoriesControl);
+    if (filtered) {
       setFilteredWords(filtered);
     }
   };
-  
+
+  const categoryProp: ICategoryFilterList = {
+    categories: categoriesControl,
+    callback: categoryToggleCallbackFunction,
+  };
+
   const handleChangeA = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIsCheckedA(e.target.checked);
   };
-  
+
   const handleChangeB = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIsCheckedB(e.target.checked);
   };
@@ -57,36 +78,10 @@ const Dictionary = () => {
     setIsCheckedC(e.target.checked);
   };
 
-
   return (
     <div>
       <div className="">
         <Header>Diccionario</Header>
-        <div className="block m-auto w-3/4 py-8 ">
-          <label htmlFor="">Filtros</label>
-          
-        <Checkbox
-            handleChange={handleChangeA}
-            isChecked={isCheckedA}
-            label="Educación"
-          />
-          <Checkbox
-            handleChange={handleChangeB}
-            isChecked={isCheckedB}
-            label="Jurídico"
-          />
-        
-        
-          <Checkbox
-            handleChange={handleChangeC}
-            isChecked={isCheckedC}
-            label="Psicología"
-          />
-        </div>
-         
-        
-        
-          
         <div className="block m-auto w-3/4 py-8">
           <DictionarySearchInput
             handleSearch={handleSearch}
@@ -94,6 +89,7 @@ const Dictionary = () => {
             history={history}
             searchQuery={searchQuery}
             searchButtonRef={searchButtonRef}
+            categories={categoryProp}
           />
           <DictionaryList words={filteredWords} />
         </div>
